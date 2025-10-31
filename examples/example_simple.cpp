85f8d988ebed3eaba6fe9092aa3958c53b2c1830
Maxence <maxence.lemoine29@gmail.com>
2025-10-31 12:55:38 +0100

    correction du formatage de example_simple.cpp

--- examples/example_simple.cpp
+++ examples/example_simple.cpp
@@ -1,25 +1,25 @@
 #include "genetic/genetic.hpp"
 
 int main(int argc, char** argv) {
-  using maConfigType = genetic::Config<double, uint32_t, 1, 1>;
+    using maConfigType = genetic::Config<double, uint32_t, 1, 1>;
 
-  maConfigType config;
+    maConfigType config;
 
-  genetic::GeneticAlgorithm<maConfigType> ga{
-      config,
-      [](const std::vector<std::vector<double>>& vecs) -> maConfigType::real_type {
-        double sum = 0.0;
-        for (const auto& vec : vecs) {
-          for (double val : vec) {
-            sum += val;
-          }
-        }
-        return (sum * sum);  // Negative because we want to minimize
-      },
-      0};
+    genetic::GeneticAlgorithm<maConfigType> ga{
+        config,
+        [](const std::vector<std::vector<double>>& vecs) -> maConfigType::real_type {
+            double sum = 0.0;
+            for (const auto& vec : vecs) {
+                for (double val : vec) {
+                    sum += val;
+                }
+            }
+            return (sum * sum);  // Negative because we want to minimize
+        },
+        0};
 
-  ga.run();
+    ga.run();
 
-  std::cin.get();
-  return 0;
+    std::cin.get();
+    return 0;
 }
